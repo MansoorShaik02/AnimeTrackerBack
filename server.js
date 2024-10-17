0a79f0d48b47fd1a7a8f8544f34fa27d8e3af8e4
mansoor <mansoorshaik7b@gmail.com>
2024-10-17 15:57:02 +0530

    done

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config('{path:D:/reactproectsreal/MERNAnimeDB/MERN-AnimeTracker/backend/.env}');
+
+
+const app = express();
+
+// Connect to database
+connectDB();
+app.use(cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
+// Middleware
+
+app.use(express.json());
+//haha
+// Routes
+
+app.use('/api/users', userRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
